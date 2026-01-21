264c977c4985dcbecd261e112e2f8ab5f9f31239
shingoooooon <skksb75@gmail.com>
2026-01-21 13:10:25 +0900

    update server config

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from "./app"
 
 const PORT = 3000
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on port :${PORT}`)
 })
